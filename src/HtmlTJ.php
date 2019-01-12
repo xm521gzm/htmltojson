065815cf6649db43fee504634d5bf829266887ba
jhonny <470047253@qq.com>
2019-01-12 20:29:57 +0800

    html to json

--- src/HtmlTJ.php
+++ src/HtmlTJ.php
@@ -12,15 +12,110 @@ namespace HtmlTj;
  */
 class HtmlToJson
 {
+    public $text_tag = ['body',"html","meta","head","br"];
+    public $text_tag_2 = ['strong',"span","b","h3"];
+
 
     /**
      * @return string
      */
-    public function toJson()
+    public function toJson($html)
     {
-        return "Hello World!";
+
+        $ret = $this->html_to_obj($html);
+        return $ret;
+        //$this->dump($ret);
+    }
+
+
+    function html_to_obj($html) {
+        //$this->dump($html);
+        $dom = new \DOMDocument();
+        $libxml_previous_state = libxml_use_internal_errors(true);
+        $dom->loadHTML("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />".$html);
+
+        libxml_clear_errors();
+        libxml_use_internal_errors($libxml_previous_state);
+        $data = $this->element_to_obj($dom->documentElement,$ret);
+
+        return($ret);
+
+        //return $data;
+    }
+
+    function element_to_obj($element,&$ret) {
+        //dump($element);
+        //return;
+        $obj = array( "tag" => $element->tagName );
+        foreach ($element->attributes as $attribute) {
+            $obj[$attribute->name] = $attribute->value;
+
+        }
+
+
+        //$ret[] = $obj;
+        $noadd = 1;
+        foreach ($element->childNodes as $subElement) {
+
+
+            if ($subElement->nodeType == XML_TEXT_NODE) {
+
+                $obj['html'] = $subElement->wholeText;
+                $ret[] = $obj;
+                $noadd = 0 ;
+
+
+            }
+            else {
+
+                $this->element_to_obj($subElement,$ret);
+                //$obj["children"][] =$tmp;
+            }
+        }
+
+
+
+        // 去除无用元素
+        if(in_array($obj['tag'],$this->text_tag)){
+            $noadd = 0;
+        }
+        // 去除无用元素
+        if(in_array($obj['tag'],$this->text_tag_2) && !isset($obj['html'])){
+            $noadd = 0;
+        }
+
+
+        if($noadd == 1){
+            $ret[] = $obj;
+
+        }
+
+
+
+        return $obj;
+    }
+
+    function img_to_obj($element){
+
+        foreach ($element->attributes as $attribute) {
+            $o[$attribute->name] = $attribute->value;
+        }
+        require $o;
+    }
+
+    /**
+     * 打印函数
+     *
+     */
+
+    function dump($var, $exit = true) {
+        echo '<pre>';
+        print_r ( $var );
+        echo '</pre>';
 
     }
+
+
 
 
 }
--- tests/HtmlTJTest.php
+++ tests/HtmlTJTest.php
@@ -1,6 +1,28 @@
 <?php
 
-class HtmlTJTest extends PHPUnit_Framework_TestCase
-{
+require "../src/HtmlTj.php";
+use HtmlTj\HtmlToJson;
+$htj = new HtmlToJson();
+// 微信内容地址
+$testurl ="https://mp.weixin.qq.com/s?__biz=MzAwMDM4Mjg2Nw%3D%3D&mid=2650432558&idx=1&sn=09520d25a65a7c2727d6d11bac60d4ca&scene=45#wechat_redirect";
+$html = file_get_contents($testurl);
+// 正则匹配
+preg_match("/<div class=\"rich_media_content \" id=\"js_content\">(.*?)<\/div>/ism",$html,$arcmat);
+
+//html 转json
+
+$ret = $htj->toJson($arcmat[1]);
+dump($ret);
+/**
+ * 打印函数
+ *
+ */
+
+function dump($var, $exit = true) {
+    echo '<pre>';
+    print_r ( $var );
+    echo '</pre>';
 
 }
+//$url ="https://mp.weixin.qq.com/s?src=11&timestamp=1547276051&ver=1317&signature=dbwag6YCfhHCVfBjszAcNwHpOUIqvvSGh3adjxbAc0L4kpmDW1qQYHm9iZk0G56qi6gC1Unxkr-WFd9J0oJQgYbEohTeyo6D6Lr8guJ1qkvCX7FTm8IpbiQ6OIVmjGiN&new=1";
+
